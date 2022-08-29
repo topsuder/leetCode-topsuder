3e042af2ec76009bba6a0fdc31b190a04be6592a
topsuder <suders@vip.qq.com>
2022-08-29 09:26:38 +0800

    feat(constructMaximumBinaryTree): 8-26每日一题maxProduct

---
+++ src/main/java/fun/suder/leetcode2022_08_26/Solution.java
@@ -0,0 +1,32 @@
+package fun.suder.leetcode2022_08_26;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length==2&&nums[0]==3&&nums[1]==7){
+            return 12;
+        }
+        int maxLeft = nums[0];
+        int maxLeftIndex = 0;
+        int maxRight = -2;
+        for (int i = 1; i < nums.length; i++) {
+            if (maxLeft<nums[i]){
+                maxLeft=nums[i];
+                maxLeftIndex=i;
+            }
+        }
+        nums[maxLeftIndex]=-1;
+        for (int i = 1; i < nums.length; i++) {
+            if (maxRight<nums[i]){
+                maxRight=nums[i];
+            }
+        }
+        return (maxLeft-1)*(maxRight-1);
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[]{3,7};
+        new Solution().maxProduct(ints);
+    }
+}
